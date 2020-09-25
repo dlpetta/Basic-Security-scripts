bbc0687c983df98d865ffe47f83102ef90f33a0c
dlpetta <50181435+dlpetta@users.noreply.github.com>
2020-09-25 19:01:06 -0300

    Add files via upload

--- reverso.sh
+++ reverso.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 for ip in $(seq 1 254);do
-host -t ptr 37.59.174.$ip | grep -v "37.59.174" | cut -d " " -f 5 | grep -v "NXDOMAIN"
+host -t ptr 132.148.90.$ip | grep -v "132.148.90" | cut -d " " -f 5 | grep -v "NXDOMAIN"
 
 done
